a75b892c2c093b1fdf9d0812a6a13024005944d6
Prashant Srivastava <prashantsrivastava381@gmail.com>
2021-07-19 21:24:51 +0530

    Portfolio create

---
+++ JavaScript/index.js
@@ -0,0 +1,16 @@
+$("#jquery-btn1").on("click",function(){
+    window.location = "https://prashantirs.github.io/Payment-Gateway-Integration_Project/" ;
+})
+
+$("#jquery-btn2").on("click",function(){
+    window.location = "" ;
+})
+
+$("#jquery-btn3").on("click",function(){
+    window.location = "" ;
+})
+
+
+
+
+   
